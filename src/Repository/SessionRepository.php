d430379ba7b0bdc6a0100c86ecd34251df96f05f
Ben0it-T <37017213+Ben0it-T@users.noreply.github.com>
2025-11-23 00:18:44 +0100

    Update SessionRepository

--- src/Repository/SessionRepository.php
+++ src/Repository/SessionRepository.php
@@ -7,10 +7,10 @@ use PDO;
 
 final class SessionRepository
 {
-    private $pdo;
+    private PDO $pdo;
     private int $lifetime;
 
-    public function __construct(PDO $pdo, $lifetime) {
+    public function __construct(PDO $pdo, int $lifetime) {
         $this->pdo = $pdo;
         $this->lifetime = $lifetime;
 
@@ -25,57 +25,69 @@ final class SessionRepository
         );
     }
 
-    public function _open() {
+    public function _open(): bool {
         $this->_gc($this->lifetime);
         return true;
     }
 
-    public function _close() {
+    public function _close(): bool {
         //$this->pdo = null;
         return true;
     }
 
-    public function _read($id) {
+    public function _read($id): string {
         $id = preg_replace('/[^a-zA-Z0-9,\-]/', '', $id);
-        $stmt = $this->pdo->prepare('SELECT `data` FROM `tacos_sessions` WHERE `tacos_sessions`.`id` = ?');
-        $stmt->execute([$id]);
+        $stmt = $this->pdo->prepare('SELECT `data` FROM `tacos_sessions` WHERE `tacos_sessions`.`id` = :id LIMIT 1');
+        $stmt->execute([
+            'id' => $id
+        ]);
         $res = $stmt->fetch();
 
         if ($res) {
             if (empty($res['data'])) {
                 return ""; // Return an empty string
             } else {
-                return $res['data'];
+                return (string) $res['data'];
             }
         } else {
             return ""; // Return an empty string
         }
     }
 
-    public function _write($id, $data) {
+    public function _write($id, $data): bool {
         $id = preg_replace('/[^a-zA-Z0-9,\-]/', '', $id);
         $time = time(); // Create time stamp
-        $stmt = $this->pdo->prepare('REPLACE INTO `tacos_sessions` (`tacos_sessions`.`id`, `tacos_sessions`.`data`, `tacos_sessions`.`time`) VALUES (?, ?, ?)');
-        $stmt->execute([$id, $data, $time]);
+        $sql  = 'INSERT INTO `tacos_sessions` (`tacos_sessions`.`id`, `tacos_sessions`.`data`, `tacos_sessions`.`time`)  VALUES (:id, :data, :time) ';
+        $sql .= 'ON DUPLICATE KEY UPDATE `tacos_sessions`.`data` = VALUES(data), `tacos_sessions`.`time` = VALUES(time)';
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([
+            'id' => $id,
+            'data' => $data,
+            'time' => $time
+        ]);
 
         return true;
     }
 
-    public function _destroy($id) {
+    public function _destroy($id): bool {
         $id = preg_replace('/[^a-zA-Z0-9,\-]/', '', $id);
-        $stmt = $this->pdo->prepare('DELETE FROM `tacos_sessions` WHERE `tacos_sessions`.`id` = ?');
-        $stmt->execute([$id]);
+        $stmt = $this->pdo->prepare('DELETE FROM `tacos_sessions` WHERE `tacos_sessions`.`id` = :id');
+        $stmt->execute([
+            'id' => $id
+        ]);
         return true;
     }
 
-    public function _gc($lifetime) {
+    public function _gc($lifetime): bool {
         $expire = time() - intval($lifetime);
-        $stmt = $this->pdo->prepare('DELETE FROM `tacos_sessions` WHERE `tacos_sessions`.`time` < ?');
-        $stmt->execute([$expire]);
+        $stmt = $this->pdo->prepare('DELETE FROM `tacos_sessions` WHERE `tacos_sessions`.`time` < :expire');
+        $stmt->execute([
+            'expire' => $expire
+        ]);
 
-        $stmt = $this->pdo->query('DELETE FROM `tacos_sessions` WHERE `tacos_sessions`.`data` = ""');
+        $stmt = $this->pdo->prepare('DELETE FROM `tacos_sessions` WHERE `tacos_sessions`.`data` = "" OR `tacos_sessions`.`data` IS NULL');
+        $stmt->execute();
 
         return true;
     }
-
 }
