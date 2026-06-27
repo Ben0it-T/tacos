d949d9882b35d97f62231102ea28e56f49548a4c
Ben0it-T <37017213+Ben0it-T@users.noreply.github.com>
2026-06-27 07:51:01 +0200

    refactor: move infra to container

--- config/container.php
+++ config/container.php
@@ -1,10 +1,110 @@
 <?php
 declare(strict_types=1);
 
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+
+use Slim\Csrf\Guard;
+use Slim\Views\Twig;
 
 return [
     // Application settings
     'settings' => fn() => require __DIR__ . '/settings.php',
+
+    //
+    // Infra
+    //
+
+    'translations' => function (ContainerInterface $c) {
+        $settings = $c->get('settings')['lang'];
+        $translations = require sprintf("%s/%s.php", $settings['path'], 'en_US');
+
+        // Overwrite default lang with local lang
+        $langFile = sprintf('%s/%s.php', $settings['path'], $settings['default']);
+        if (file_exists($langFile)) {
+            $localLang = require $langFile;
+            $translations  = array_merge($translations, $localLang);
+        }
+
+        return $translations;
+    },
+
+    PDO::class => function (ContainerInterface $c) {
+        $db = $c->get('settings')['db'];
+        $dsn = sprintf(
+            'mysql:host=%s;port=%s;dbname=%s;charset=%s',
+            $db['host'],
+            $db['port'],
+            $db['dbname'],
+            $db['charset']
+        );
+        return new PDO($dsn, $db['user'], $db['pass'], $db['options']);
+    },
+
+    Twig::class => function (ContainerInterface $c) {
+        $settings = $c->get('settings')['twig'];
+        $cache = is_bool($settings['cache']) === true ? (bool)$settings['cache'] : sprintf("%s", $settings['cache']);
+        $twig  = Twig::create(sprintf("%s", $settings['templates']), [
+            'cache' => $cache,
+            'charset' => 'UTF-8',
+            'strict_variables' => true,
+            'autoescape' => 'html',
+        ]);
+
+        // Twig global vars
+        $basepath = is_null($c->get('settings')['app']['basepath']) ? '/' : sprintf("%s", $c->get('settings')['app']['basepath']);
+        $twig->getEnvironment()->addGlobal('basePath', $basepath);
+        $twig->getEnvironment()->addGlobal('trans', $c->get('translations'));
+
+        return $twig;
+    },
+
+    PHPMailer::class => function (ContainerInterface $c) {
+        $settings = $c->get('settings')['mailer'];
+        $mailer = new PHPMailer();
+        if ($settings['mode'] == 'smtp') {
+            $mailer->isSMTP();
+            $mailer->SMTPDebug = $settings['smtp_debug'];
+            $mailer->SMTPAuth = (bool)$settings['smtp_auth'];
+            $mailer->Host = sprintf("%s", $settings['smtp_host']);
+            $mailer->Port = intval($settings['smtp_port']);
+            $mailer->Username = sprintf("%s", $settings['smtp_user']);
+            $mailer->Password = sprintf("%s", $settings['smtp_pass']);
+            $mailer->SMTPSecure = $settings['smtp_secure'];
+        } else if ($settings['mode'] == 'sendmail') {
+            $mailer->isSendmail();
+        }
+        $mailer->setFrom(sprintf("%s", $settings['from_addr']), sprintf("%s", $settings['from_name']));
+        $mailer->CharSet = sprintf("%s", $settings['charset']);
+        $mailer->Encoding = 'base64';
+        $mailer->isHTML(true);
+
+        return $mailer;
+    },
+
+    LoggerInterface::class => function (ContainerInterface $c) {
+        $settings = $c->get('settings')['logger'];
+
+        $handler = new RotatingFileHandler(
+            $settings['path'] . '/app.log',
+            $settings['maxFiles'],
+            $settings['level'],
+            true,
+            0640
+        );
+        $handler->setFormatter(new LineFormatter(null, null, false, true));
+
+        $logger = new Logger('app');
+        $logger->pushHandler($handler);
+
+        return $logger;
+    },
 
 
 ];
